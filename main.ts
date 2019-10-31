5f7f3c43c505ef2ad2bd7b1f07abbf6ec0ccb071
mrpenguin <sadcebu2003@yahoo.com>
2019-10-30 20:28:34 -0500

    Added tiles to background. Working on movement.

--- main.ts
+++ main.ts
@@ -1,3 +1,12 @@
+controller.up.onEvent(ControllerButtonEvent.Pressed, function () {
+    if (playerMoving != true) {
+        playerMoving = true
+    }
+})
+scene.onHitTile(SpriteKind.Player, 12, function (sprite) {
+    playerMoving = false
+})
+let playerMoving = false
 let mySprite = sprites.create(img`
 . d . d b b b . 
 . d . b f f c . 
@@ -8,26 +17,6 @@ d b c b b b b b
 . . . b c c c . 
 . . . b . . c . 
 `, SpriteKind.Player)
-scene.setTile(0, img`
-7 7 7 7 7 7 7 7 
-7 7 7 7 6 7 7 7 
-7 7 7 7 7 7 7 7 
-7 7 7 7 7 7 7 7 
-7 6 7 7 7 7 7 7 
-7 7 7 7 7 7 7 7 
-7 7 7 7 7 7 7 6 
-7 7 7 7 7 7 7 7 
-`, false)
-scene.setTile(0, img`
-7 9 9 6 6 6 7 7 
-9 6 6 7 7 7 9 9 
-9 7 7 7 6 6 7 6 
-6 7 7 6 7 7 7 6 
-7 6 7 7 7 7 7 7 
-6 7 7 7 b b b 6 
-6 7 b b 6 6 6 6 
-c 6 6 6 6 6 6 c 
-`, false)
 scene.setTileMap(img`
 d d d d d d d d d d d d d d d d d d d d 
 d d d d d 5 d d 5 d d 5 d d 5 d d d d d 
@@ -47,3 +36,63 @@ c 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 c
 `, TileScale.Eight)
 mySprite.x = 8 * 10 + 4
 mySprite.y = scene.screenHeight() - 4
+scene.setTile(12, img`
+7 c c 6 6 6 7 7 
+c 6 6 7 7 7 9 c 
+c 7 7 7 6 6 7 6 
+6 7 7 6 7 7 7 6 
+7 6 7 7 7 7 7 7 
+6 7 7 7 b b b 6 
+6 7 b b 6 6 6 6 
+c c c c c c c c 
+`, false)
+scene.setTile(7, img`
+7 7 7 7 7 7 7 7 
+7 7 7 7 6 7 7 7 
+7 7 7 7 7 7 7 7 
+7 7 7 7 7 7 7 7 
+7 6 7 7 7 7 7 7 
+7 7 7 7 7 7 7 7 
+7 7 7 7 7 7 7 6 
+7 7 7 7 7 7 7 7 
+`, false)
+scene.setTile(11, img`
+4 4 4 4 4 4 d 4 
+4 5 4 4 4 4 4 4 
+4 4 4 4 4 4 4 4 
+4 d 4 4 4 4 4 5 
+4 4 4 5 4 4 4 4 
+4 4 4 4 4 4 4 4 
+4 5 4 4 4 d 4 4 
+4 4 4 4 4 4 4 4 
+`, false)
+scene.setTile(6, img`
+6 6 6 6 6 6 6 6 
+9 9 6 9 9 6 6 9 
+6 6 6 6 6 6 6 6 
+6 9 9 9 6 9 9 9 
+6 6 6 6 6 6 6 6 
+9 9 6 9 6 9 9 6 
+6 6 6 6 6 6 6 6 
+6 9 6 6 9 9 6 6 
+`, false)
+scene.setTile(5, img`
+f f f f f f f f 
+f f f f f f f f 
+f f f f f f f f 
+f f f f f f f f 
+f f f f f f f f 
+f f f f f f f f 
+f f f f f f f f 
+f f f f f f f f 
+`, false)
+scene.setTile(13, img`
+b b b b b b b b 
+d d d d d b d d 
+b b b b b b b b 
+d d b d d d d d 
+b b b b b b b b 
+d d d d d b d d 
+b b b b b b b b 
+d d b d d d d d 
+`, false)
